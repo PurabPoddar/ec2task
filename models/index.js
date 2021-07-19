893317fb6cb5066e461741c96a1e8dc115ec13e8
PurabPoddar <79688018+PurabPoddar@users.noreply.github.com>
2021-07-19 19:06:29 +0530

    config commit

--- models/index.js
+++ models/index.js
@@ -7,6 +7,8 @@ const basename = path.basename(__filename);
 const {config} = require('../config/config');
 const db = {};
 
+console.log(config);
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
